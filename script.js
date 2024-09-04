072750978d2680df244e0632c439aab8720630cc
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2024-09-04 19:07:16 +0200

    cleanup

--- script.js
+++ script.js
@@ -1,40 +1,38 @@
 const button = document.getElementById('createZip');
 const fileInput = document.getElementById('fileInput');
-
 const preview = document.getElementById('preview');
 const previewContainer = document.getElementById('previewContainer');
 const previewImage = document.getElementById('previewImage');
 
-const packMeta = `{
-    "pack": {
-        "pack_format": 1,
-        "description": "§eSkyHanni Custom Scoreboard Background"
+const packMeta = JSON.stringify({
+    pack: {
+        pack_format: 1,
+        description: "§eSkyHanni Custom Scoreboard Background"
     }
-}`;
+}, null, 2);
 
-button.addEventListener('click', async function () {
+button.addEventListener('click', () => {
     if (fileInput.files.length === 0) {
         alert('Please select a PNG file.');
         return;
     }
 
     const file = fileInput.files[0];
-
     const fileReader = new FileReader();
 
-    fileReader.onload = function (event) {
+    fileReader.onload = (event) => {
         const zip = new JSZip();
 
         zip.file("pack.mcmeta", packMeta);
 
-        // Create assets directory structure and add the selected PNG, renaming it to scoreboard.png
+        // Create assets directory structure and add the selected PNG
         const assetsFolder = zip.folder("assets");
         const skyhanniFolder = assetsFolder.folder("skyhanni");
         skyhanniFolder.file("scoreboard.png", event.target.result.split(',')[1], { base64: true });
 
         // Generate the zip and trigger download
         zip.generateAsync({ type: "blob" })
-            .then(function (content) {
+            .then((content) => {
                 const a = document.createElement('a');
                 a.href = URL.createObjectURL(content);
                 a.download = 'SkyHanni Scoreboard Background.zip';
@@ -45,48 +43,44 @@ button.addEventListener('click', async function () {
     fileReader.readAsDataURL(file);
 });
 
-
-fileInput.addEventListener('change', function () {
+fileInput.addEventListener('change', () => {
     if (fileInput.files.length === 0) {
         button.style.display = "none";
-        preview.style.display = "none";
+        preview.classList.add('hidden');
     } else {
         button.style.display = "block";
-        preview.style.display = "block";
-        previewContainer.style.width = "275px";
-        previewContainer.style.height = "500px";
+        preview.classList.remove('hidden');
         previewImage.src = URL.createObjectURL(fileInput.files[0]);
     }
 });
 
-document.getElementById('howto').addEventListener('click', function () {
+document.getElementById('instructions').addEventListener('click', function () {
     this.classList.toggle('active');
 });
 
-previewContainer.addEventListener("mousedown", function (e) {
+previewContainer.addEventListener("mousedown", (e) => {
     const startX = e.clientX;
     const startY = e.clientY;
-
     const startWidth = previewContainer.offsetWidth;
     const startHeight = previewContainer.offsetHeight;
 
-    function doDrag(e) {
+    const doDrag = (e) => {
         const newWidth = startWidth - (e.clientX - startX);
         const newHeight = startHeight - (e.clientY - startY);
 
-        previewContainer.style.width = newWidth + "px";
-        previewContainer.style.height = newHeight + "px";
+        previewContainer.style.width = `${newWidth}px`;
+        previewContainer.style.height = `${newHeight}px`;
         previewImage.style.width = "100%";
         previewImage.style.height = "100%";
-    }
+    };
 
-    function stopDrag() {
+    const stopDrag = () => {
         document.removeEventListener("mousemove", doDrag, false);
         document.removeEventListener("mouseup", stopDrag, false);
-    }
+    };
 
     document.addEventListener("mousemove", doDrag, false);
     document.addEventListener("mouseup", stopDrag, false);
 
     e.preventDefault();
-}, false);
+});
